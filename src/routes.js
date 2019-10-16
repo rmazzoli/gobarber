e4c7a35a90a17d97b47bf680ad6b007f5e7ba5f0
rmazzoli <rafael.mazzoli@hotmail.com>
2019-10-16 00:10:19 -0300

    chore: add User store sript

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,8 @@
 import { Router } from 'express';
+import UserController from './app/controllers/UserController';
 
-const routes = new Router(); 
+const routes = new Router();
 
-routes.get('/', (req, res)=>{
-  return res.json({message: "Helo World!"});
-});
+routes.post('/users', UserController.store);
 
 export default routes;
