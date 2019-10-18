c073aa6cf59acfab89bb7fc42e4f77816cc55aee
rmazzoli <rafael.mazzoli@hotmail.com>
2019-10-17 23:50:46 -0300

    feat: add upload file

---
+++ src/database/migrations/20191018021610-create-files.js
@@ -0,0 +1,33 @@
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('files', {
+      id: {
+        type: Sequelize.INTEGER,
+        allowNull: true,
+        autoIncrement: true,
+        primaryKey: true,
+      },
+      name: {
+        type: Sequelize.STRING,
+        allowNull: true,
+      },
+      path: {
+        type: Sequelize.STRING,
+        allowNull: true,
+        unique: true,
+      },
+      created_at: {
+        type: Sequelize.DATE,
+        allowNull: true,
+      },
+      updated_at: {
+        type: Sequelize.DATE,
+        allowNull: true,
+      },
+    });
+  },
+
+  down: queryInterface => {
+    return queryInterface.dropTable('files');
+  },
+};
